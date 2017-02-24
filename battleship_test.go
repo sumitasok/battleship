99cbda9b918b2f87fd5eb095ad397bbbaa269455
Sumit Asok <sumitasok@gmail.com>
2017-02-25 00:11:12 +0530

    more tests for ship count

--- battleship_test.go
+++ battleship_test.go
@@ -26,4 +26,12 @@ func TestValidateShipCount(t *testing.T) {
 	if validateShipCount(10, 5) != nil {
 		t.Errorf("validation `false negative` caught allowed ship count to be wrong")
 	}
+
+	if validateShipCount(9, 5) == nil {
+		t.Errorf("validation failed to catch maximum allowed ship")
+	}
+
+	if validateShipCount(9, 4) != nil {
+		t.Errorf("validation `false negative` caught allowed ship count to be wrong")
+	}
 }
